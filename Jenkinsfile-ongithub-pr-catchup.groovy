8795b7dcdf25c4c9fe50c2416dc1f033107644c4
Regis PAINBLANC <rpainblanc@gmail.com>
2022-09-09 14:54:35 +0200

    Log

--- Jenkinsfile-ongithub-pr-catchup.groovy
+++ Jenkinsfile-ongithub-pr-catchup.groovy
@@ -152,6 +152,7 @@ pipeline {
                                     }
                                     //job.scheduleBuild(0, new hudson.model.Cause.UserIdCause("jenkins"))
                                     def tm_events = getGitHubPRIssueTimelineEvents(repository_owner, repository_name, env.GITHUB_PASSWORD, pr.number)
+                                    log_message(messages, "Job exists for PR ${pr.number} (${pr.pull_request.url}) and last build is id=${last_build.id}, last_exec=${last_build.startTimeInMillis}")
                                     // Scan the timeline events received **after** the last build start time
                                     def tm_events_to_check = []
                                     for (event in tm_events) {
