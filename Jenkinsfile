0f5268bde5a70dbf47ff506f4ad8f15c6732fcd4
Regis PAINBLANC <rpainblanc@gmail.com>
2022-09-09 14:32:19 +0200

    Add Git commit hash in description

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,6 @@
 node('built-in') {
+    def commit_id = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+    currentBuild.description = "Git hash: ${commit_id}"
     deleteDir()
     sh 'printenv > printenv.txt | sort'
     sleep 5
--- Jenkinsfile-ongithub-pr-catchup.groovy
+++ Jenkinsfile-ongithub-pr-catchup.groovy
@@ -110,6 +110,8 @@ def log_message(def messages, def message) {
 }
 
 node('built-in') {
+    def commit_id = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+    currentBuild.description = "Git hash: ${commit_id}"
     deleteDir()
     def messages = []
     withCredentials([
