63f25c8394330511aec372b7b766b3af6c6ef85a
Regis PAINBLANC <rpainblanc@gmail.com>
2022-09-14 11:10:27 +0200

    Other transient

--- Jenkinsfile-ongithub-pr-catchup.groovy
+++ Jenkinsfile-ongithub-pr-catchup.groovy
@@ -17,11 +17,11 @@ pipeline {
                             for (pr_number in prs) {
                                 def jenkins_directory_name = 'dip-on-github-pr'
                                 def job_name = "${jenkins_directory_name}/PR-${pr_number}"
-                                transient def job = Jenkins.get().getItemByFullName(job_name)
+                                def transient job = Jenkins.get().getItemByFullName(job_name)
                                 def tm_events = jenkins_lib_groovy.getGitHubPRIssueTimelineEvents(env.GITHUB_PASSWORD, pr_number)
                                 def pr_issue_comments = jenkins_lib_groovy.getGitHubPRIssueComments(env.GITHUB_PASSWORD, pr_number)
                                 def builder_template = jenkins_lib_groovy.findBuilderTemplateInGithubIssueComments(pr_issue_comments)
-                                job = null
+                                //job = null
                                 writeJSON file: "PR-${pr_number}-github-issue-events.json", json: tm_events
                                 writeJSON file: "PR-${pr_number}-github-issue-comments.json", json: pr_issue_comments
                                 writeJSON file: "PR-${pr_number}-builder-template.json", json: builder_template
