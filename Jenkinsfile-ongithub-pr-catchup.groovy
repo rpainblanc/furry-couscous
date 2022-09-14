b6eb119ed7c7eef5a5f186e98426fe77e57ec426
Regis PAINBLANC <rpainblanc@gmail.com>
2022-09-14 11:07:26 +0200

    Loop on PRs

--- Jenkinsfile-ongithub-pr-catchup.groovy
+++ Jenkinsfile-ongithub-pr-catchup.groovy
@@ -12,16 +12,22 @@ pipeline {
                     def jenkins_lib_groovy = load 'lib.groovy'
                     withCredentials([
                             usernamePassword(credentialsId: 'jenkins-dataiku', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_PASSWORD')]) {
-                        def jenkins_directory_name = 'dip-on-github-pr'
-                        def job_name = "${jenkins_directory_name}/PR-${pr.number}"
-                        def job = Jenkins.get().getItemByFullName(job_name)
-                        def tm_events = jenkins_lib_groovy.getGitHubPRIssueTimelineEvents(env.GITHUB_PASSWORD, "17873")
-                        def pr_issue_comments = jenkins_lib_groovy.getGitHubPRIssueComments(env.GITHUB_PASSWORD, "17873")
-                        def builder_template = jenkins_lib_groovy.findBuilderTemplateInGithubIssueComments(pr_issue_comments)
-                        writeJSON file: 'github-issue-events.json', json: tm_events
-                        writeJSON file: 'github-issue-comments.json', json: pr_issue_comments
-                        writeJSON file: 'builder-template.json', json: builder_template
-                        archiveArtifacts artifacts: '*.json', allowEmptyArchive: true
+                        def prs = ["17873"]
+                        try {
+                            for (pr_number in prs) {
+                                def jenkins_directory_name = 'dip-on-github-pr'
+                                def job_name = "${jenkins_directory_name}/PR-${pr_number}"
+                                def job = Jenkins.get().getItemByFullName(job_name)
+                                def tm_events = jenkins_lib_groovy.getGitHubPRIssueTimelineEvents(env.GITHUB_PASSWORD, pr_number)
+                                def pr_issue_comments = jenkins_lib_groovy.getGitHubPRIssueComments(env.GITHUB_PASSWORD, pr_number)
+                                def builder_template = jenkins_lib_groovy.findBuilderTemplateInGithubIssueComments(pr_issue_comments)
+                                writeJSON file: "PR-${pr_number}-github-issue-events.json", json: tm_events
+                                writeJSON file: "PR-${pr_number}-github-issue-comments.json", json: pr_issue_comments
+                                writeJSON file: "PR-${pr_number}-builder-template.json", json: builder_template
+                            }
+                        } finally {
+                            archiveArtifacts artifacts: '*.json', allowEmptyArchive: true
+                        }
                     }
                 }
             }
